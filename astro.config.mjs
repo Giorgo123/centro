b9b7c3db77d57991a695a864a352b0a6fec5fbc6
Giorgo123 <gonzaloo-georgetti@hotmail.com.ar>
2024-05-28 13:00:46 -0300

    averrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stately-gumdrop-a6d22f.netlify.app/', // Reemplaza con tu URL de Netlify
   outDir: 'dist',
-  adapter: netlify()
+  adapter: netlify(),
+  build: {
+    site: 'https://main--stately-gumdrop-a6d22f.netlify.app'
+  }
 });
