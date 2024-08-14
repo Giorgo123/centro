9cd21c0a1475ca7d9a8d0dcac378da67f68ac682
Giorgo123 <gonzaloo-georgetti@hotmail.com.ar>
2024-08-14 18:16:22 -0300

    Incluyendo public_html y actualizando .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://stately-gumdrop-a6d22f.netlify.app/', // Reemplaza con tu URL de Netlify
-  outDir: 'dist',
-  adapter: netlify(),
-  build: {
-    site: 'https://main--stately-gumdrop-a6d22f.netlify.app'
-  }
-});
+
+// astro.config.mjs
+export default {
+  buildOptions: {
+    outDir: './public_html',
+  },
+};
